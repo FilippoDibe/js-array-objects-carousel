d979261eb4d37b0ebbe853626a135666a9d85c16
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-12-11 18:05:38 +0100

    fix bug

--- script.js
+++ script.js
@@ -1,84 +1,79 @@
 const images = [
-    {   image: 'img/01.webp', 
-        title: 'Marvel\'s Spiderman Miles Morale', 
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     },
-    {   
-        image: 'img/02.webp', 
+    {
+        image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.', 
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     },
-    { 
-        image: 'img/03.webp', 
-        title: 'Fortnite', 
+    {
+        image: 'img/03.webp',
+        title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     },
-    { 
+    {
         image: 'img/04.webp',
-        title: 'Stray', 
+        title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     },
-    { 
-        image: 'img/05.webp', 
-        title: "Marvel's Avengers", 
+    {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
 
-const items = document.getElementsByClassName('item');
-let activeIndex = 0;
+let currentIndex = 0;
 
-const next = document.querySelector("#next");
-const back = document.querySelector("#back");
+function showSlide(index) {
+    const mainImage = document.getElementById('mainImage');
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
 
-// Popola il carosello con le immagini dall'array
-images.forEach((img, index) => {
-    const item = document.createElement('div');
-    item.classList.add('item');
-    if (index === 0) {
-        item.classList.add('active');
+    mainImage.src = images[index].image;
+    mainImage.alt = images[index].title;
+    title.textContent = images[index].title;
+    description.textContent = images[index].text;
+
+    const thumbnails = document.getElementsByClassName('thumb');
+    for (let i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].classList.remove('active');
     }
-
-    const thumbnail = document.createElement('div');
-    thumbnail.classList.add('thumbnail');
-    thumbnail.innerHTML = `<img src="${img.image}" alt="${img.title}">`;
-    thumbnail.addEventListener('click', () => showImage(index));
-
-    item.innerHTML = `
-        <img src="${img.image}" alt="${img.title}">
-        <div class="info">
-            <h2>${img.title}</h2>
-            <p>${img.text}</p>
-        </div>
-    `;
-
-    items[index] = item;
-    document.querySelector('.container-items').appendChild(item);
-    document.querySelector('.thumbnails-container').appendChild(thumbnail);
-});
-
-// Aggiungi il comportamento del carosello
-next.addEventListener('click', showNext);
-back.addEventListener('click', showPrev);
-
-function showNext() {
-    updateCarousel((activeIndex + 1) % items.length);
+    thumbnails[index].classList.add('active');
 }
 
-function showPrev() {
-    updateCarousel((activeIndex - 1 + items.length) % items.length);
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showSlide(currentIndex);
 }
 
-function showImage(index) {
-    updateCarousel(index);
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showSlide(currentIndex);
 }
 
-function updateCarousel(index) {
-    items[activeIndex].classList.remove('active');
-    document.querySelector('.thumbnails-container').querySelector('.thumbnail.active-thumbnail').classList.remove('active-thumbnail');
+function initThumbnails() {
+    const thumbnailContainer = document.getElementById('thumbnailContainer');
 
-    activeIndex = index;
+    images.forEach((image, index) => {
+        const thumb = document.createElement('div');
+        thumb.classList.add('thumb');
+        thumb.innerHTML = `<img src="${image.image}" alt="${image.title}">`;
+        thumb.addEventListener('click', () => {
+            currentIndex = index;
+            showSlide(currentIndex);
+        });
+        thumbnailContainer.appendChild(thumb);
+    });
 
-    items[activeIndex].classList.add('active');
-    document.querySelector('.thumbnails-container').querySelectorAll('.thumbnail')[activeIndex].classList.add('active-thumbnail');
+    const firstThumbnail = thumbnailContainer.firstChild;
+    if (firstThumbnail) {
+        firstThumbnail.classList.add('active');
+    }
 }
+
+initThumbnails();
+showSlide(currentIndex);
